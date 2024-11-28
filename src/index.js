e56b616162886bb2b9c81b6ec66b2eced27b652b
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2024-11-28 13:33:10 +0300

    feat: add navigation functionality to nav buttons for navigating between home and menu

--- src/index.js
+++ src/index.js
@@ -2,7 +2,39 @@ import './css/styles.css';
 import renderHome from './modules/home';
 import renderMenu from './modules/menu';
 
-document.addEventListener("DOMContentLoaded", e => {
-    // renderHome();
-    renderMenu();
-});
+const navButtons = document.querySelectorAll(".nav-el button");
+let activeNav = document.querySelector("li.nav-el:first-child")
+const pageContent = document.getElementById("content");
+
+navButtons.forEach(button => {
+    button.addEventListener("click", handleNavButtonClick);
+})
+
+renderHome();
+
+function handleNavButtonClick(e) {
+    activeNav.classList.toggle("active");
+
+    const liElement = e.target.closest("li.nav-el");
+
+    liElement.classList.toggle("active");
+    activeNav = liElement;
+
+    pageContent.innerHTML = "";
+    
+    const navData = liElement.getAttribute("data-nav");
+    switch (navData) {
+        case "home":
+            renderHome();
+            break;
+        case "menu":
+            renderMenu();
+            break;
+        default:
+            return;
+    }
+    
+    window.scrollTo({
+        top: 0
+    });
+}
